190e05e7f0ec1131e9c9c442fdc18984b2e82ccf
Metrinenaliaka <metrinenam@gmail.com>
2023-10-05 09:03:46 -0700

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    length = len(sys.argv) - 1
+    symbols = {"+": add, "-": sub, "*":mul, "/": div}
+    if length != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] not in list(symbols.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, symbols[sys.argv[2]](a, b)))
