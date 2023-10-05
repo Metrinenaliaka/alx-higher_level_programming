16e38072d5ae46aa9df089a746fd38317a4f1570
Metrinenaliaka <metrinenam@gmail.com>
2023-10-05 06:01:26 -0700

    fixed bugs

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(f"{a:d} + {b:d} = {add(a,b):d}")
-    print(f"{a:d} - {b:d} = {sub(a,b):d}")
-    print(f"{a:d} * {b:d} = {mul(a,b):d}")
-    print(f"{a:d} / {b:d} = {div(a,b):d}")
+    ad = add(a, b)
+    su = sub(a, b)
+    mu = mul(a, b)
+    di = div(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, ad))
+    print("{:d} - {:d} = {:d}".format(a, b, su))
+    print("{:d} * {:d} = {:d}".format(a, b, mu))
+    print("{:d} / {:d} = {:d}".format(a, b, di))
