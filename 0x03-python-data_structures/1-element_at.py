f144412d782326e05de24fbfb33e1f7537c8f9e2
Metrinenaliaka <metrinenam@gmail.com>
2023-10-07 10:58:47 -0700

    fixed errors

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    length = len(my_list)
+    length = len(my_list) - 1
     if idx < 0 or idx > length:
         return None
     return my_list[idx]
