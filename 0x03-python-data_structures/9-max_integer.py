392d44397f8e66e7ffb146ab7d32e92c8ede7fdc
Metrinenaliaka <metrinenam@gmail.com>
2023-10-08 03:30:45 -0700

    maximum integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    bignumber = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > bignumber:
+            bignumber = my_list[i]
+    return (big)
