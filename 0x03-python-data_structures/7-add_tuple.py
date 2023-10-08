5ac9f8b35e7ed5cd6509ef829ac47c0239004e18
Metrinenaliaka <metrinenam@gmail.com>
2023-10-08 03:21:46 -0700

    fixed errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,5 +12,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    new = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return new
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
