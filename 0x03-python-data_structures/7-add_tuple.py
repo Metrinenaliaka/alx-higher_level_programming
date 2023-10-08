65caa03cc7d4e8eec26eee637e95eb66d7dd132a
Metrinenaliaka <metrinenam@gmail.com>
2023-10-08 03:10:12 -0700

    adding tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    al = len(tuple_a)
+    bl = len(tuple_b)
+    if al < 2:
+        if al == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if bl < 2:
+        if bl == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+    new = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new
