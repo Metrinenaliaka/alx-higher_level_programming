46b8ba57968b22079f37ab48601fa97b19c686c3
Metrinenaliaka <metrinenam@gmail.com>
2023-10-08 02:27:44 -0700

    corrected errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = [i for i in my_string if i != 'c' and i != 'C']
-    return (''.join(new_string))
+    new_string = str()
+    for ch in my_string:
+        if (ch != 'C' and ch != 'c'):
+            new_string = new_string + ch
+    return (new_string)
