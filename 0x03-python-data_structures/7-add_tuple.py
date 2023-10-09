3ab371ae9258478efa36cfa661f664ba4a8daed3
Metrinenaliaka <metrinenam@gmail.com>
2023-10-09 00:36:45 -0700

    fixed errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    al = len(tuple_a)
-    bl = len(tuple_b)
-    if al == 0:
+    if len(tuple_a) == 0:
         tuple_a = 0, 0
-    elif al == 1:
+    elif len(tuple_a) == 1:
         tuple_a = tuple_a[0], 0
-    if bl == 0:
+    if len(tuple_b) == 0:
         tuple_b = 0, 0
-    elif bl == 1:
+    elif len(tuple_b) == 1:
         tuple_b = tuple_b[0], 0
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
