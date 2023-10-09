77d9286a15c5ba34d019674e7cb623d05187e443
Metrinenaliaka <metrinenam@gmail.com>
2023-10-09 00:44:39 -0700

    fixed errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
-        print("")
+        for idx, element in enumerate(row):
+            print("{:d}".format(element), end="")
+            if (idx < len(row) - 1):
+                print("{}".format(" "), end="")
+        print()
