2329e5bafb43f905f2389ef218469acc8c706da5
Metrinenaliaka <metrinenam@gmail.com>
2023-10-11 07:55:35 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,4 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        a_dictionary.items()
-    return max(a_dictionary.keys())
+        return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
