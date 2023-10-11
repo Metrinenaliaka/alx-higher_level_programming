29438a54a044e2bcfb46fa07f879ece48e068526
Metrinenaliaka <metrinenam@gmail.com>
2023-10-11 06:23:50 -0700

    fixed errors

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    s_keys = dict(sorted(a_dictionary.items()))
-    for key, value in s_keys.items():
-        print(f"{key} : {value}")
+    sorted_dict = dict(sorted(a_dictionary.items()))
+    for key, value in sorted_dict.items():
+        print(f"{key}: {value}")
