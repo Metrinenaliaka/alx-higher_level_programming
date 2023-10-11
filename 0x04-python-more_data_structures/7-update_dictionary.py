af11b568ec3d1354f8e6dd5e7b1a4cb6f21d69cf
Metrinenaliaka <metrinenam@gmail.com>
2023-10-11 05:55:10 -0700

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary == True:
+    if (key in a_dictionary) is True:
         a_dictionary[key] = value
     else:
         a_dictionary[key] = value
