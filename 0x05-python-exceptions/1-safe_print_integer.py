818a3926651767630063b77bae150a7a468ca6b5
Metrinenaliaka <metrinenam@gmail.com>
2023-10-23 11:19:49 -0700

    prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value = int(value):
+        if value == int(value):
             print("{:d}".format(value))
             return True
-    except Exception as ValueError and TypeError:
+    except Exception as e:
         return False
