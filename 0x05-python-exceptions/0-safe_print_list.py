134098a3594fb0e31f4bd01c5ebbd19495e8d0b7
Metrinenaliaka <metrinenam@gmail.com>
2023-10-23 10:45:25 -0700

    corrected errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for x in range(0, x):
             print(my_list[x], end="")
             count += 1
-    except:
+    except Exception:
         pass
     print()
     return count
