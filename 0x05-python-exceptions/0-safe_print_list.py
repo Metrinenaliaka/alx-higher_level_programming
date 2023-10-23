22818befeaf05138e0c0c50698ed60c2d22456cd
Metrinenaliaka <metrinenam@gmail.com>
2023-10-23 07:50:20 -0700

    printing elements

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for x in range(0, x):
+            print(my_list[x], end="")
+            count += 1
+    except:
+        pass
+    print()
+    return count
