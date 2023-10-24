53c62ab43b9ed4374dd5b436f477dee36679cf05
Metrinenaliaka <metrinenam@gmail.com>
2023-10-24 09:01:14 -0700

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 class Square:
+    """class with setter and getter"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
