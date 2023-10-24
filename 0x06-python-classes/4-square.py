cae6b2a5c789df54139615156097cbf1650e9de8
Metrinenaliaka <metrinenam@gmail.com>
2023-10-24 11:29:07 -0700

    property, setters and getters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
+"""creates a square and determines area"""
+
+
 class Square:
+    """creates a square and determines area"""
     def __init__(self, size=0):
-        self.size = size
+        """initializes square
+        Args:
+            size: size of square"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """determines area of square"""
+        res = self.__size ** 2
+        return res
 
     @property
     def size(self):
+        """gets size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """sets size"""
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = value
-
-    def area(self):
-        return self.__size ** 2
