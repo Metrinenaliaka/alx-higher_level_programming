cdf5f3cc61ec2af05e4567f10fe9f5cc26ee1cbf
Metrinenaliaka <metrinenam@gmail.com>
2023-10-30 04:43:44 -0700

    private properties

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Defines A Rectangle with height and width
+Args:
+    height: int/float
+    width: in/float
+"""
+
+
+class Rectangle:
+    """class with private atrributes"""
+    def __init__(self, width=0, height=0):
+        """defining width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """gets width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width"""
+        if not isinstance(value, int) and not isinstance(value, float):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """gets height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height"""
+        if not isinstance(value, int) and not isinstance(value, float):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
