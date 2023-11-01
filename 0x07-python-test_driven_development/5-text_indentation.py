f3ba48c286c4556120b99c5d524a4ebfe22c64eb
Metrinenaliaka <metrinenam@gmail.com>
2023-10-31 18:24:45 -0700

    more tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,6 +8,13 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     length = len(text)
+    flag = True
     for i in range(length):
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+        if flag and text[i].isspace():
+            continue
+        print(text[i], end='')
+        if text[i] in ['.', '?', ':']:
             print("\n")
+            flag = True
+        else:
+            flag = False
