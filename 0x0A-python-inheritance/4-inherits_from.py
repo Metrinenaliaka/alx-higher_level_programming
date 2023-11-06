30917f18c133c3bd45e58002d828c51e26ed552c
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 05:46:41 -0800

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if object is an instance of a class inherited from a_class
+    Args
+        obj: object
+        a_class: class
+    Return: True or False"""
+    if not type(obj) == a_class and issubclass(type(obj), a_class):
+        return (True)
+    else:
+        return (False)
