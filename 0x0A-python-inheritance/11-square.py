610115ad8c4a525d351f997ec2eb188a85fc1dab
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 06:29:19 -0800

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -19,4 +19,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """returns dimension of square"""
-        return('[Square] {}/{}'.format(self.__size, self.__size))
+        return ('[Square] {}/{}'.format(self.__size, self.__size))
