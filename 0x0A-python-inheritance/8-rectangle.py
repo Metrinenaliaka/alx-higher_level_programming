02adba7f76ad85083760dd2378422204d2f4658c
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 06:16:00 -0800

    inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""inheritance"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """initializer
+        Args
+           width
+           height
+        """
+        if super().integer_validator('width', width):
+            self.__width = width
+        if super().integer_validator('height', height):
+            self.__height = height
