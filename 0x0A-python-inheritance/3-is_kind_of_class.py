70658e8cfdff899b41d80393b9cac5f53a8abbbe
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 05:42:09 -0800

    class inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""inheritance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if obj is an instance of a class inherited from a_class
+    Args
+       obj: object
+       a_class: class
+    Return: True or False"""
+    if isinstance(obj, a_class):
+        return(True)
+    return (False)
