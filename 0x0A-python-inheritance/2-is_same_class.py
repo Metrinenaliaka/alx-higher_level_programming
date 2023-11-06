0f1a7598dd2ff5ed2f59008397b66d520a0ef587
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 05:38:14 -0800

    same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""inheritance"""
+
+
+def is_same_class(obj, a_class):
+    """checks if object is exactly an instance of a_class
+    Args
+       obj: object
+       a_class: class
+    """
+    return (type(obj) == a_class)
