90dff31e3cc2209a3010ceac2eadf05dfcaf9dd6
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 06:34:34 -0800

    inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""inheritance"""
+
+
+class MyInt(int):
+    """class utilizing int class"""
+
+    def __eq__(self, other):
+        """initializer for eq"""
+        if int.__ne__(self, other):
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        """intializer for greater than"""
+        if int.__eq__(self, other):
+            return True
+        else:
+            return False
