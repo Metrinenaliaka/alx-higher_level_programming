4e6db07e6974f80613378a14fe24f824bc3088d9
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 06:25:57 -0800

    super function

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""inheritance"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+
+    def __init__(self, size):
+        """initalizer
+        Args
+           size: size of side of square
+        """
+        super().integer_validator('size', size)
+        Rectangle.__init__(self, size, size)
