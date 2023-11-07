e235ef4a1eb19bcc3bc15acfe211c0fa49acbb6f
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 22:26:36 -0800

    writes to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""writes information to a file"""
+
+
+def write_file(filename="", text=""):
+    """filename = file to be written to, text"""
+    with open('filename', 'w', encoding='utf-8') as f_name:
+        read_chars = f_name.write(text)
+        return read_chars
+
