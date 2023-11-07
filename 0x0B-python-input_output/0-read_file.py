a3baccee551601c24e0b87229b8c75c9b3b4574a
Metrinenaliaka <metrinenam@gmail.com>
2023-11-07 03:55:51 -0800

    changes made

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""function reads from textfile"""
+"""opens file to read"""
 
 
 def read_file(filename=""):
-    """filename = file to be opened"""
-    with open("filename", encoding='UTF8') as myfile:
-        read_file = myfile.read()
-        print(read_file, end='')
+    """filename = file"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
