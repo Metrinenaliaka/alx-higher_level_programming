daa3c6c239077dff270eada30c0573a908f4437b
Metrinenaliaka <metrinenam@gmail.com>
2023-11-07 05:26:42 -0800

    pascals triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists of lists"""
+
+
+def pascal_triangle(n):
+    """n = integer"""
+    matrix = []
+    if n <= 0:
+        return (matrix)
+
+    for i in range(n):
+        matrix.append([])
+        matrix[i].append(1)
+        for j in range(1, i):
+            matrix[i].append(matrix[i - 1][j - 1] + matrix[i - 1][j])
+        if (i != 0):
+            matrix[i].append(1)
+    return (matrix)
