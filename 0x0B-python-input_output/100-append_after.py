667eeb7bc8858fa2c937a970d645aa35c4d7f210
Metrinenaliaka <metrinenam@gmail.com>
2023-11-07 09:17:49 -0800

    append after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""appending"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append after line if search string is in line
+    Args
+       filename - file
+       search_string - string to search for
+       new_string - new string to append after if search is found
+    """
+    with open(filename, 'r', encoding='UTF8') as f:
+        tmp = f.readlines()
+
+    with open(filename, 'w', encoding='UTF8') as f:
+        for line in tmp:
+            if search_string in line:
+                line = line + new_string
+            f.write(line)
