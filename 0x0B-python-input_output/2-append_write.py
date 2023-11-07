ed4d5cdfc8c698b171c74b5c55a1be532e56c121
Metrinenaliaka <metrinenam@gmail.com>
2023-11-06 22:33:05 -0800

    append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""appends to a file"""
+
+
+def append_write(filename="", text=""):
+    """
+    Args:
+        filename: file to be appended
+        text: text to append
+    """
+    with open('filename', 'a', encoding="utf-8") as f:
+        append_f = f.write(text)
+        return append_f
