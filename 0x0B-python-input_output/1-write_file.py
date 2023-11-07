786fb30ab4977f45f8bfa4c13d9da08ad830d63b
Metrinenaliaka <metrinenam@gmail.com>
2023-11-07 03:58:38 -0800

    changes made

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,7 @@
 
 def write_file(filename="", text=""):
     """filename = file to be written to, text"""
-    with open('filename', 'w', encoding='utf-8') as f_name:
+    with open(filename, 'w', encoding='utf-8') as f_name:
         read_chars = f_name.write(text)
         return (read_chars)
 
