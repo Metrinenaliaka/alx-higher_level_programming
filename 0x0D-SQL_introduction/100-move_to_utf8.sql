980be7175d9fcb006de2e8b4df2bc003aa4e16ba
Metrinenaliaka <128808519+Metrinenaliaka@users.noreply.github.com>
2023-12-13 20:38:30 +0300

    Update 100-move_to_utf8.sql
    changed syntax

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,4 @@
 -- Convert the database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF8
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the field to UTF8
-ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ USE `hbtn_0c_0`
+   ALTER TABLE `first_table`
+   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
