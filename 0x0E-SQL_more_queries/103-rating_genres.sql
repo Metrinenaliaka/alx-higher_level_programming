346dbde3656f28c753f594ce679d9c15d5ad85d7
Metrinenaliaka <128808519+Metrinenaliaka@users.noreply.github.com>
2023-12-14 16:18:53 +0300

    Update 103-rating_genres.sql
    fixed errors

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- script that lists all genres in the database
 -- hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, SUM(tv_shows.rating) AS rating_sum
+SELECT tv_genres.name, SUM( tv_show_genres.tv_show_ratings) AS rating_sum
 FROM tv_genres
 JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
 JOIN tv_shows ON tv_shows_genres.show_id = tv_shows.id
