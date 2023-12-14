22922b96ae7645495c6007037f6aa300700a172c
Metrinenaliaka <128808519+Metrinenaliaka@users.noreply.github.com>
2023-12-14 15:03:55 +0300

    Update 0-privileges.sql
    Coorected errors

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- shows priviledges of a suer
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1 @localhost;
+SHOW GRANTS FOR user_0d_2 @localhost;
