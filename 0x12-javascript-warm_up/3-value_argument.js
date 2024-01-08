306c096f630265397537779e4e042bc0253ec7cc
Metrinenaliaka <metrinenam@gmail.com>
2024-01-08 01:32:15 -0800

    control flow

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * script that prints the first argument passed to it
+ */
+'use strict';
+const args = process.argv.length;
+if (args < 3){
+	console.log('No argument');
+} else if (args === 3){
+	console.log(process.argv[2]);
+} else{
+	console.log(process.argv[2]);
+}
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/**
+ * prints two arguments passed to it, in the following format: “ is ”
+ */
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(`${arg1} is ${arg2}`);
