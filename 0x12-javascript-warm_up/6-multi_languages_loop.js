3ceef7eca5223fabe964841227d75b5af7edd49d
Metrinenaliaka <metrinenam@gmail.com>
2024-01-08 02:20:05 -0800

    loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * printing all 3 languages
+ */
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(languages[i]);
+  i++;
+}
