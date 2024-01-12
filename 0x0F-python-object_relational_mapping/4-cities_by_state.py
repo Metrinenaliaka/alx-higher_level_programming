38caf817443fe28b3ad020cecb8969454c38bb2c
Metrinenaliaka <metrinenam@gmail.com>
2024-01-12 02:56:35 -0800

    joining two tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""lists all cities from database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+
+def main(argv):
+    """main function"""
+
+    if len(argv) - 1 != 3:
+        print("Must enter 4 arguments")
+        return
+
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+    FROM cities \
+    INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
+
+    for row in cur.fetchall():
+        print(row)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
