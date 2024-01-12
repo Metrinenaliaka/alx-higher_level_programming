59c887f2c9c100d9fde1ffba61c0589ab2e46623
Metrinenaliaka <metrinenam@gmail.com>
2024-01-12 03:12:24 -0800

    creating table using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class definition of a State and an instance Base = declarative_base()"""
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """state class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        return("<States(id='%s', name='%s')>" % (self.id, self.name))
