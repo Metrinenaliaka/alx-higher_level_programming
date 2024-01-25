1f7ea72b1c852055f66c69263cdeba5ff4605c99
Metrinenaliaka <metrinenam@gmail.com>
2024-01-25 01:53:45 -0800

    fixed pycodestyle

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Makes a request to 0.0.0.0:5000/catch_me to get it!
-curl -sLX PUT --data "user_id=98" --header "origin:ALX" 0.0.0.0:5000/catch_me
+curl -sLX PUT --data "user_id=98" --header "origin:HolbertonSchool" 0.0.0.0:5000/catch_me
